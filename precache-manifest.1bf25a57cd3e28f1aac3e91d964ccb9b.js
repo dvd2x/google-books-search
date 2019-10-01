2712582bbf2a14dc521c40a9f6c3327ac28cc8cf
David Piper <black.god1930@gmail.com>
2019-10-01 15:23:25 -0400

    Updates

---
+++ precache-manifest.1bf25a57cd3e28f1aac3e91d964ccb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee77350aac75bb39bc05702fa40e3bfe",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "b0d70f6d37bcd29fe882",
+    "url": "/google-books-search/static/css/main.1ed74802.chunk.css"
+  },
+  {
+    "revision": "fec5755cec644b85d947",
+    "url": "/google-books-search/static/js/2.841d35d8.chunk.js"
+  },
+  {
+    "revision": "b0d70f6d37bcd29fe882",
+    "url": "/google-books-search/static/js/main.886f96c8.chunk.js"
+  },
+  {
+    "revision": "d672f040abfa3c0ea28a",
+    "url": "/google-books-search/static/js/runtime~main.9ad78463.js"
+  },
+  {
+    "revision": "016abe581eaea2303ae61702f526e1ed",
+    "url": "/google-books-search/static/media/bookImg.016abe58.svg"
+  }
+]);
